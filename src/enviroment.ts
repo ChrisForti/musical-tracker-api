b748b01d3d5525317febb90d922cbdc49204e42e
Christopher Forti <christopher.forti.79@gmail.com>
2025-05-07 18:39:46 -0400

    Revert "Install drizzle plus dependencies, and made a users table."

--- src/enviroment.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import assert from "assert";
-
-export const connectionString = process.env.DATABASE_URL!;
-
-assert(connectionString, "DATABASE_URL must be set.");
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import { db } from "./drizzle/db.js";
-import { sql } from "drizzle-orm";
 
 const app = express();
 
@@ -8,13 +6,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
 });
 
-app.listen(3000, async (err) => {
+app.listen(3000, (err) => {
   if (err) {
     console.error(err);
   }
-
-  const time = await db.execute(sql`SELECT NOW()`);
-  const dbTimeStamp = time.rows[0]!.now ?? "";
-  console.log("from database", dbTimeStamp);
   console.log("server starting on port 3000");
 });
