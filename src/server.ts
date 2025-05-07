db435e132df0fd6a7070361eea4335d6a7857941
Christopher Forti <christopher.forti.79@gmail.com>
2025-05-06 20:46:05 -0400

    Install drizzle plus dependencies, and made a users table.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { db } from "./drizzle/db.js";
+import { sql } from "drizzle-orm";
 
 const app = express();
 
@@ -6,9 +8,13 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
 });
 
-app.listen(3000, (err) => {
+app.listen(3000, async (err) => {
   if (err) {
     console.error(err);
   }
+
+  const time = await db.execute(sql`SELECT NOW()`);
+  const dbTimeStamp = time.rows[0]!.now ?? "";
+  console.log("from database", dbTimeStamp);
   console.log("server starting on port 3000");
 });
