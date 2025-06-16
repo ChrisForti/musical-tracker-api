3f6dcea6930acdaf96800e11630e1d0e0e38f906
Christopher Forti <christopher.forti.79@gmail.com>
2025-06-16 19:34:12 -0400

    Add uuid to casting handler

--- src/v1/routes/castings.ts
+++ src/v1/routes/castings.ts
@@ -5,6 +5,7 @@ import { db } from "../../drizzle/db.js";
 import { SERVER_ERROR } from "../../lib/errors.js";
 import { and, eq } from "drizzle-orm";
 import { ensureAdmin, ensureAuthenticated } from "../../lib/auth.js";
+// import { validate as validateUuid } from "uuid";
 
 export const castingRouter = Router();
 
@@ -25,28 +26,37 @@ async function getAllCastingsHandler(req: Request, res: Response) {
 }
 
 type CreateCastingBodyParams = {
-  roleId: string | number;
-  actorId: string | number;
-  performanceId: string | number;
+  roleId: string;
+  actorId: string;
+  performanceId: string;
 };
 
 async function createCastingHandler(
   req: Request<{}, {}, CreateCastingBodyParams>,
   res: Response
 ) {
-  const roleId = Number(req.body.roleId);
-  const actorId = Number(req.body.actorId);
-  const performanceId = Number(req.body.performanceId);
+  const roleId = req.body.roleId;
+  const actorId = req.body.actorId;
+  const performanceId = req.body.performanceId;
   const validator = new Validator();
 
   try {
-    validator.check(!isNaN(roleId) && roleId > 0, "roleId", "is required");
-    validator.check(!isNaN(actorId) && actorId > 0, "actorId", "is required");
-    validator.check(
-      !isNaN(performanceId) && performanceId > 0,
-      "performanceId",
-      "is required"
-    );
+    validator.check(!!roleId, "roleId", "is required");
+    if (roleId) {
+      validator.check(validateUuid(roleId), "id", "must be a valid UUID");
+    }
+    validator.check(!!actorId, "actorId", "is required");
+    if (actorId) {
+      validator.check(validateUuid(actorId), "id", "must be a valid UUID");
+    }
+    validator.check(!!performanceId, "performanceId", "is required");
+    if (performanceId) {
+      validator.check(
+        validateUuid(performanceId),
+        "id",
+        "must be a valid UUID"
+      );
+    }
 
     if (!validator.valid) {
       res.status(400).json({ errors: validator.errors });
@@ -76,9 +86,9 @@ async function createCastingHandler(
 }
 
 type GetCastingByKeyParams = {
-  roleId: string | number;
-  actorId: string | number;
-  performanceId: string | number;
+  roleId: string;
+  actorId: string;
+  performanceId: string;
 };
 
 async function getCastingByIdHandler(
@@ -86,27 +96,25 @@ async function getCastingByIdHandler(
   res: Response
 ) {
   const { roleId, actorId, performanceId } = req.params;
-  const rId = Number(roleId);
-  const aId = Number(actorId);
-  const pId = Number(performanceId);
   const validator = new Validator();
 
   try {
-    validator.check(
-      !isNaN(rId) && rId > 0,
-      "roleId",
-      "is required and must be a number"
-    );
-    validator.check(
-      !isNaN(aId) && aId > 0,
-      "actorId",
-      "is required and must be a number"
-    );
-    validator.check(
-      !isNaN(pId) && pId > 0,
-      "performanceId",
-      "is required and must be a number"
-    );
+    validator.check(!!roleId, "roleId", "is required");
+    if (roleId) {
+      validator.check(validateUuid(roleId), "id", "must be a valid UUID");
+    }
+    validator.check(!!actorId, "actorId", "is required");
+    if (actorId) {
+      validator.check(validateUuid(actorId), "id", "must be a valid UUID");
+    }
+    validator.check(!!performanceId, "performanceId", "is required");
+    if (performanceId) {
+      validator.check(
+        validateUuid(performanceId),
+        "id",
+        "must be a valid UUID"
+      );
+    }
 
     if (!validator.valid) {
       res.json({ errors: validator.errors });
@@ -115,9 +123,9 @@ async function getCastingByIdHandler(
 
     const result = await db.query.CastingTable.findFirst({
       where: and(
-        eq(CastingTable.roleId, rId),
-        eq(CastingTable.actorId, aId),
-        eq(CastingTable.performanceId, pId)
+        eq(CastingTable.roleId, roleId),
+        eq(CastingTable.actorId, actorId),
+        eq(CastingTable.performanceId, performanceId)
       ),
     });
 
@@ -134,24 +142,24 @@ async function getCastingByIdHandler(
 }
 
 type UpdateCastinghBodyParams = {
-  roleId: string | number;
-  actorId: string | number;
-  performanceId: string | number;
+  roleId: string;
+  actorId: string;
+  performanceId: string;
 };
 
 async function updateCastingHandler(
   req: Request<{}, {}, UpdateCastinghBodyParams>,
   res: Response
 ) {
-  const roleId = Number(req.body.roleId);
-  const actorId = Number(req.body.actorId);
-  const performanceId = Number(req.body.performanceId);
+  const roleId = req.body.roleId;
+  const actorId = req.body.actorId;
+  const performanceId = req.body.performanceId;
   const validator = new Validator();
 
   type UpdatedData = {
-    roleId?: number;
-    actorId?: number;
-    performanceId?: number;
+    roleId?: string;
+    actorId?: string;
+    performanceId?: string;
   };
 
   try {
@@ -193,36 +201,37 @@ async function updateCastingHandler(
 }
 
 type DeleteCastingBodyParams = {
-  roleId: string | number;
-  actorId: string | number;
-  performanceId: string | number;
+  roleId: string;
+  actorId: string;
+  performanceId: string;
 };
 
 async function deleteCastingHandler(
   req: Request<{}, {}, DeleteCastingBodyParams>,
   res: Response
 ) {
-  const roleId = Number(req.body.roleId);
-  const actorId = Number(req.body.actorId);
-  const performanceId = Number(req.body.performanceId);
+  const roleId = req.body.roleId;
+  const actorId = req.body.actorId;
+  const performanceId = req.body.performanceId;
   const validator = new Validator();
 
   try {
-    validator.check(
-      !isNaN(roleId) && roleId > 0,
-      "roleId",
-      "is required and must be a number > 0"
-    );
-    validator.check(
-      !isNaN(actorId) && actorId > 0,
-      "actorId",
-      "is required and must be a number > 0"
-    );
-    validator.check(
-      !isNaN(performanceId) && performanceId > 0,
-      "performanceId",
-      "is required and must be a number > 0"
-    );
+    validator.check(!!roleId, "roleId", "is required");
+    if (roleId) {
+      validator.check(validateUuid(roleId), "id", "must be a valid UUID");
+    }
+    validator.check(!!actorId, "actorId", "is required");
+    if (actorId) {
+      validator.check(validateUuid(actorId), "id", "must be a valid UUID");
+    }
+    validator.check(!!performanceId, "performanceId", "is required");
+    if (performanceId) {
+      validator.check(
+        validateUuid(performanceId),
+        "id",
+        "must be a valid UUID"
+      );
+    }
 
     if (!validator.valid) {
       res.json({ errors: validator.errors });
@@ -252,4 +261,7 @@ async function deleteCastingHandler(
     console.error("Error in deleteCastingHandler:", error);
     res.status(500).json({ error: SERVER_ERROR });
   }
+}
+function validateUuid(rId: string): boolean {
+  throw new Error("Function not implemented.");
 }
