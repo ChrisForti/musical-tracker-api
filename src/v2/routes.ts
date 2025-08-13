ee4c84a5a4ad3e1b4558c72c70f01aaca9700634
Christopher Forti <christopher.forti.79@gmail.com>
2025-08-13 19:47:20 -0400

    start file structure for version 2

---
+++ src/v2/routes.ts
@@ -0,0 +1,20 @@
+// import { Router } from "express";
+// import { userRouter } from "./routes/users.js";
+// import { theaterRouter } from "./routes/theaters.js";
+// import { roleRouter } from "./routes/roles.js";
+// import { actorRouter } from "./routes/actors.js";
+// import { musicalRouter } from "./routes/musicals.js";
+// import { performanceRouter } from "./routes/performances.js";
+// import { castingRouter } from "./routes/castings.js";
+// import { productionRouter } from "./routes/productions.js";
+
+// export const v1Router = Router(); // Can add auth logig here too encompass whole router
+
+// v1Router.use("/user", userRouter);
+// v1Router.use("/theater", theaterRouter);
+// v1Router.use("/role", roleRouter);
+// v1Router.use("/actor", actorRouter);
+// v1Router.use("/musical", musicalRouter);
+// v1Router.use("/performance", performanceRouter);
+// v1Router.use("/casting", castingRouter);
+// v1Router.use("/production", productionRouter);
